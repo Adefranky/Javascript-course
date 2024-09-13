7d9fe1ccd228266d8b47ad422912e59b5b1740d2
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-09-13 20:49:58 +0300

    variables and numbers

---
+++ app.js
@@ -0,0 +1,63 @@
+// Variables
+// (reserved keyword) (variable name) (type of value)
+// declare a variable
+// let name;
+
+// assign a value to the variable
+// var and let allows you to change the value type as in the case of frankDev below
+let banana;
+banana = "fruit"
+console.log("banana")
+
+let frankDev = "mrbeast";
+frankDev = "champion"
+console.log(frankDev);
+
+// using const will not allow you to change the value of the variable after declaration
+const franky = "name"
+console.log(franky);
+
+
+// convention for writing variables
+// using uppercase, lowercase, kebcase, carmelCase, snakecase
+
+
+// challenge
+let name = "frank";
+console.log(name);
+let career = "programmer";
+console.log(career);
+let gender = "male";
+console.log(gender);
+let twitterHandle = "frank254";
+console.log(twitterHandle);
+
+// Primitive types
+// 1. Number
+let num = 10;
+console.log(num);
+
+let float = 10.4
+console.log(typeof float);
+
+console.log(2 - 2);
+console.log(2 ** 2);
+
+let counter = 10;
+counter++
+console.log(counter);
+
+counter--
+console.log(counter);
+
+// challenge
+let firstFavNumber = 100;
+let secondFavNumber = 80;
+console.log(firstFavNumber + secondFavNumber);
+console.log(firstFavNumber - secondFavNumber);
+console.log(firstFavNumber * secondFavNumber);
+console.log(firstFavNumber / secondFavNumber);
+console.log(firstFavNumber % secondFavNumber)
+console.log(firstFavNumber ** secondFavNumber);
+
+// Booleans
