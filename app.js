cd1c69117dabd80714096251c0ac80b8ff5e5093
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-09-16 20:39:28 +0300

    conditional statements

--- app.js
+++ app.js
@@ -205,5 +205,59 @@ mone = parseFloat(mone);
 console.log(mone);
 console.log(typeof mone);
 
+// controll flow
+// if ===money
+// buy some stuff
+// else !===money
+// bring some money dude
+
+// if (condition) {...}
+// else if (condition) {...}
+// else {}
+
+// conditional statements
+
+let a = 20;
+let b = 20;
+if (a > b) {
+    console.log("a is greater than b")
+} else if (a < b) {
+    console.log("a is less than b");
+} else {
+    console.log('a is equal to b')
+}
+
+let time = 10;
+let greetings;
+if (time < 10) {
+    console.log(greetings)
+
+} else if (time < 20) {
+    greetings = "good day";
+
+    console.log(greetings);
+
+} else {
+    greetings = "good evening";
+    console.log(greetings);
+}
 
 
+// Challenge
+let password;
+if (password === 8) {
+    // password = "welcome";
+    console.log("welcome");
+} else if (password <= 8) {
+
+    console.log("password is too short");
+} else if (password >= 8) {
+
+    console.log("too long password password");
+    console.log("should be 8 characters");
+} else {
+
+    console.log("please provide a password");
+}
+
+// switch statements
